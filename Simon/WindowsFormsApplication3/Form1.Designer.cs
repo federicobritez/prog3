5605e2bcf649320e1b49074e1e354ec99eb4d706
federicobritez <fedebritez@gmail.com>
2016-06-09 19:44:38 -0300

    Add files via upload

---
+++ Simon/WindowsFormsApplication3/Form1.Designer.cs
@@ -0,0 +1,184 @@
+﻿namespace WindowsFormsApplication3 {
+    partial class Form1 {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.buttonAz = new System.Windows.Forms.Button();
+            this.buttonAm = new System.Windows.Forms.Button();
+            this.buttonRo = new System.Windows.Forms.Button();
+            this.buttonVe = new System.Windows.Forms.Button();
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.buttonReset = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // buttonAz
+            // 
+            this.buttonAz.BackColor = System.Drawing.Color.Navy;
+            this.buttonAz.FlatAppearance.BorderSize = 0;
+            this.buttonAz.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonAz.Location = new System.Drawing.Point(117, 45);
+            this.buttonAz.Name = "buttonAz";
+            this.buttonAz.Size = new System.Drawing.Size(65, 42);
+            this.buttonAz.TabIndex = 0;
+            this.buttonAz.UseVisualStyleBackColor = false;
+            this.buttonAz.Click += new System.EventHandler(this.buttonAz_Click);
+            this.buttonAz.MouseClick += new System.Windows.Forms.MouseEventHandler(this.buttonAz_MouseClick);
+            this.buttonAz.MouseUp += new System.Windows.Forms.MouseEventHandler(this.buttonAz_MouseUp);
+            // 
+            // buttonAm
+            // 
+            this.buttonAm.BackColor = System.Drawing.Color.DarkGoldenrod;
+            this.buttonAm.FlatAppearance.BorderSize = 0;
+            this.buttonAm.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonAm.Location = new System.Drawing.Point(107, 141);
+            this.buttonAm.Name = "buttonAm";
+            this.buttonAm.Size = new System.Drawing.Size(75, 55);
+            this.buttonAm.TabIndex = 1;
+            this.buttonAm.UseVisualStyleBackColor = false;
+            this.buttonAm.Click += new System.EventHandler(this.buttonAm_Click);
+            this.buttonAm.MouseClick += new System.Windows.Forms.MouseEventHandler(this.buttonAm_MouseClick);
+            this.buttonAm.MouseUp += new System.Windows.Forms.MouseEventHandler(this.buttonAm_MouseUp);
+            // 
+            // buttonRo
+            // 
+            this.buttonRo.BackColor = System.Drawing.Color.Maroon;
+            this.buttonRo.FlatAppearance.BorderSize = 0;
+            this.buttonRo.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonRo.Location = new System.Drawing.Point(45, 83);
+            this.buttonRo.Name = "buttonRo";
+            this.buttonRo.Size = new System.Drawing.Size(64, 42);
+            this.buttonRo.TabIndex = 2;
+            this.buttonRo.UseVisualStyleBackColor = false;
+            this.buttonRo.Click += new System.EventHandler(this.buttonRo_Click);
+            this.buttonRo.MouseClick += new System.Windows.Forms.MouseEventHandler(this.buttonRo_MouseClick);
+            this.buttonRo.MouseUp += new System.Windows.Forms.MouseEventHandler(this.buttonRo_MouseUp);
+            // 
+            // buttonVe
+            // 
+            this.buttonVe.BackColor = System.Drawing.Color.DarkGreen;
+            this.buttonVe.FlatAppearance.BorderSize = 0;
+            this.buttonVe.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonVe.Location = new System.Drawing.Point(193, 91);
+            this.buttonVe.Name = "buttonVe";
+            this.buttonVe.Size = new System.Drawing.Size(75, 45);
+            this.buttonVe.TabIndex = 3;
+            this.buttonVe.UseVisualStyleBackColor = false;
+            this.buttonVe.Click += new System.EventHandler(this.buttonVe_Click);
+            this.buttonVe.MouseClick += new System.Windows.Forms.MouseEventHandler(this.buttonVe_MouseClick);
+            this.buttonVe.MouseUp += new System.Windows.Forms.MouseEventHandler(this.buttonVe_MouseUp);
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.Location = new System.Drawing.Point(25, 210);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(75, 23);
+            this.buttonStart.TabIndex = 5;
+            this.buttonStart.Text = "Jugar";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
+            // 
+            // buttonReset
+            // 
+            this.buttonReset.BackColor = System.Drawing.Color.Silver;
+            this.buttonReset.FlatAppearance.BorderSize = 0;
+            this.buttonReset.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonReset.Font = new System.Drawing.Font("DejaVu Sans", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonReset.Location = new System.Drawing.Point(127, 102);
+            this.buttonReset.Name = "buttonReset";
+            this.buttonReset.Size = new System.Drawing.Size(40, 23);
+            this.buttonReset.TabIndex = 6;
+            this.buttonReset.Text = "R";
+            this.buttonReset.UseVisualStyleBackColor = false;
+            this.buttonReset.Click += new System.EventHandler(this.buttonReset_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("DejaVu Sans Mono", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(152, 214);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(80, 18);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Puntaje:";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(107, 12);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(100, 12);
+            this.progressBar1.TabIndex = 8;
+            this.progressBar1.Click += new System.EventHandler(this.progressBar1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("DejaVu Sans Mono", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.DarkOrange;
+            this.label2.Location = new System.Drawing.Point(226, 214);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(17, 18);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "0";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DimGray;
+            this.ClientSize = new System.Drawing.Size(321, 266);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonReset);
+            this.Controls.Add(this.buttonStart);
+            this.Controls.Add(this.buttonVe);
+            this.Controls.Add(this.buttonRo);
+            this.Controls.Add(this.buttonAm);
+            this.Controls.Add(this.buttonAz);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonAz;
+        private System.Windows.Forms.Button buttonAm;
+        private System.Windows.Forms.Button buttonRo;
+        private System.Windows.Forms.Button buttonVe;
+        private System.Windows.Forms.Button buttonStart;
+        private System.Windows.Forms.Button buttonReset;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Simon/WindowsFormsApplication3/Form1.cs
@@ -0,0 +1,303 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace WindowsFormsApplication3 {
+
+    public enum Colores {AZUL, AMARILLO, ROJO,VERDE};
+    public enum Estados {SECUENCIA, ESPERANDO_ENTRADA, STOP, SEC_INVALIDA, NUEVO_COLOR};
+
+
+    public partial class Form1 : Form {
+
+        
+
+        Random r = new Random();
+        Timer timerSec = new Timer();
+        Timer timerEntrada = new Timer();
+        bool sw = false;
+        List<Colores> secuencia = new List<Colores>();   //Secuencia aleatoria
+        Queue<Colores> secEntrada = new Queue<Colores>();//Secuencia ingresada por el jugador
+        IEnumerator<Colores> eSecuencia;            // Un enumerator para recorrer la secuencia aleatoria
+        Colores currColor;
+        Colores lastColor;
+        Estados estado = Estados.STOP;
+        Array values = Enum.GetValues(typeof(Colores));
+        bool secHasNext = false; 
+
+
+        public Form1() {
+            InitializeComponent();
+            currColor = 0 ;
+            timerEntrada.Interval = 500;
+            timerEntrada.Tick += new EventHandler(timerEntrada_Tick);
+            timerEntrada.Enabled = false;
+
+            timerSec.Interval = 500;
+            timerSec.Tick += new EventHandler(timer_Tick);
+            
+        }
+
+        void timerEntrada_Tick(object sender, EventArgs e) {
+            switch (estado) {
+                case Estados.ESPERANDO_ENTRADA:
+                    if (!secHasNext) {
+                        //Agrego un nuevo color a la secuencia:
+                        Colores nuevo = (Colores)values.GetValue(r.Next(values.Length));
+                        secuencia.Add(nuevo);
+                        resetESecuencia();
+                        progressBar1.Maximum = secuencia.Count;
+                        estado = Estados.SECUENCIA;
+                        timerSec.Enabled = true;
+                        timerEntrada.Enabled = false;
+                        Debug.WriteLine("ESPERANDO_ENTRADA Nuevo Color  :{0}", nuevo.ToString());
+                        Debug.WriteLine("ESTADO:SECUENCIA");
+                    }
+                    break;
+
+                case Estados.SEC_INVALIDA:
+
+                    estado = Estados.STOP;
+                    Debug.WriteLine("ESTADO:SECUENCIA INVALIDA");
+                    break;
+
+                default:
+                    break;
+            }
+        }
+
+
+        /*
+         Enciendo AMARILLO
+Prox : AMARILLO
+The thread '<No Name>' (0x184) has exited with code 0 (0x0).
+Enciendo AMARILLO
+Prox : AMARILLO
+Enciendo AMARILLO
+Prox : AZUL
+Enciendo AZUL
+No mas botones - Curr: AZUL
+ESTADO:esperando entrada...
+         */
+        void timer_Tick(object sender, EventArgs e) {
+
+            switch (estado) { 
+                case Estados.SECUENCIA:
+
+                            if (secHasNext) {
+                                if (sw) {
+                                    //Enciendo el boton
+                                    progressBar1.Value += 1;
+                                    currColor = eSecuencia.Current;                                    
+                                    Debug.WriteLine("Enciendo {0}", currColor);
+                                    resaltarColor(currColor);
+                                    lastColor = currColor;
+                                    //Indico que debo apagar el boton en la prox Tick
+                                    sw = false;
+                                }
+                                else {
+                                    Debug.WriteLine("Apago : {0}", lastColor);
+                                    opacarColor(lastColor);
+                                    secHasNext = eSecuencia.MoveNext();
+                                    
+                                    sw = true;
+                                }
+                            }
+                            else {
+                                opacarColor(lastColor);
+                                Debug.WriteLine("No mas botones - Curr: {0}", currColor);
+                                //No hay mas botones, paso a otro estado
+                                estado = Estados.ESPERANDO_ENTRADA;
+                                timerEntrada.Enabled = true;
+                                timerSec.Enabled = false;
+                                resetESecuencia();
+                                Debug.WriteLine("ESTADO:esperando entrada...");
+                            }
+                            break;
+                
+                case Estados.STOP:
+                            Debug.WriteLine("ESTADO:STOP");
+                            break;
+
+                default:
+                            break;
+                        
+            }
+            
+
+        }
+        
+
+
+
+        public void opacarColor(Colores c){
+
+            switch(c){
+                case Colores.AZUL:
+                    buttonAz.BackColor = Color.Navy;
+                    break;
+                case Colores.AMARILLO:
+                    buttonAm.BackColor= Color.DarkGoldenrod;
+                    break;
+                case Colores.ROJO:
+                    buttonRo.BackColor = Color.Maroon;
+                    break;
+                case Colores.VERDE:
+                    buttonVe.BackColor = Color.DarkGreen;
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private void resaltarColor(Colores c) {
+            switch(c){
+                case Colores.AZUL:
+                    buttonAz.BackColor = Color.Blue;
+                    Console.Beep(209, 300);                 
+                    break;
+                case Colores.AMARILLO:
+                    buttonAm.BackColor= Color.Yellow;
+                    Console.Beep(252, 300);
+                    break;
+                case Colores.ROJO:
+                    buttonRo.BackColor = Color.Red;
+                    Console.Beep(310, 300);
+                    break;
+                case Colores.VERDE:                
+                    buttonVe.BackColor = Color.Green;
+                    Console.Beep(415, 300);
+                    break;
+                default:
+                    
+                    break;
+            }
+        }
+
+        
+
+        private void buttonStart_Click(object sender, EventArgs e) {
+            timerSec.Enabled = true;
+            sw = true;
+            secuencia.Clear();
+            secuencia.Add((Colores)values.GetValue(r.Next(values.Length) - 1));
+            resetESecuencia();
+            estado = Estados.SECUENCIA;
+        }
+
+        private void checkRespuesta(Colores rta) {
+
+            //Si hay valores en la secuencia
+            if (secHasNext) {
+                //Comparo el boton presionado con el que valor de la secuencia.
+                if (rta == eSecuencia.Current) {
+                    progressBar1.Value += 1;
+                    secEntrada.Enqueue(eSecuencia.Current);
+                    Debug.WriteLine("CheckRespuesta OK : {0} = {1}", rta.ToString(), eSecuencia.Current.ToString());
+                    secHasNext = eSecuencia.MoveNext();
+                }
+                else {
+                    //El último boton presionado no coincide con la secuencia... pedio :(
+                    estado = Estados.SEC_INVALIDA;
+                    Debug.WriteLine("CheckRespuesta ERROR : {0} != {1}", rta.ToString(), eSecuencia.Current.ToString());
+                    resetearJuego();
+                }
+                
+            }
+        }
+
+        private void resetESecuencia() {
+
+            progressBar1.Value = 0;
+            eSecuencia = secuencia.GetEnumerator();
+            secHasNext = eSecuencia.MoveNext();
+        }
+        /*
+        Green – 415 Hz
+Red – 310 Hz
+Yellow – 252 Hz
+Blue – 209 Hz
+        */
+        private void buttonReset_Click(object sender, EventArgs e) {
+            resetearJuego();
+        }
+
+
+
+        private void buttonVe_Click(object sender, EventArgs e) {
+            checkRespuesta(Colores.VERDE);  
+        }
+
+        private void buttonAm_Click(object sender, EventArgs e) {
+            checkRespuesta(Colores.AMARILLO);
+        }
+
+        private void buttonRo_Click(object sender, EventArgs e) {
+            checkRespuesta(Colores.ROJO);
+            
+        }
+        private void buttonAz_Click(object sender, EventArgs e) {
+            checkRespuesta(Colores.AZUL);
+        }
+
+
+        private void resetearJuego() {
+            estado = Estados.STOP;
+            timerSec.Enabled = false;
+            timerEntrada.Enabled = false;
+            secuencia.Clear();
+            resetESecuencia();
+        }
+
+
+        private void buttonAm_MouseUp(object sender, MouseEventArgs e) {
+            opacarColor(Colores.AMARILLO);
+        }
+
+        private void buttonRo_MouseUp(object sender, MouseEventArgs e) {
+            opacarColor(Colores.ROJO);
+        }
+
+        private void buttonVe_MouseUp(object sender, MouseEventArgs e) {
+            opacarColor(Colores.VERDE);
+        }
+
+        private void buttonAz_MouseUp(object sender, MouseEventArgs e) {
+            opacarColor(Colores.AZUL);
+        }
+
+
+
+        private void progressBar1_Click(object sender, EventArgs e) {
+            
+        }
+
+        private void label2_Click(object sender, EventArgs e) {
+
+        }
+
+        private void buttonAm_MouseClick(object sender, MouseEventArgs e) {
+            resaltarColor(Colores.AMARILLO);
+        }
+
+        private void buttonRo_MouseClick(object sender, MouseEventArgs e) {
+            resaltarColor(Colores.ROJO);
+        }
+
+        private void buttonAz_MouseClick(object sender, MouseEventArgs e) {
+            resaltarColor(Colores.AZUL);
+        }
+
+        private void buttonVe_MouseClick(object sender, MouseEventArgs e) {
+            resaltarColor(Colores.VERDE);
+        }
+     }
+
+  }
+
